05d74ef8a92e483d98ff2245fec7340d5cec03b7
easydong19 <ymh1353@naver.com>
2023-09-22 13:20:44 +0900

    그동안 프로젝트 한거 올림

---
+++ example/hello_world/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            body: Center(
+                child: Text(
+                  '코드팩토리',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.w800,
+                    color: Colors.blue,
+                  ),
+                )
+            ),
+        ),
+    );
+  }
+}
